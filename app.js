a74c948ce4411d1c414a0300045df09d450ea022
ChrisJin <chrisjin1@gmail.com>
2021-05-08 18:48:45 -0400

    default css, reset/clear grid Btn html JS

--- app.js
+++ app.js
@@ -1,31 +1,26 @@
-//? create a 16 x 16 grid of square divs
-//?     FOR loop to generate 256 divs 
-//?     create element div 
-//?     append to container parent div node
-//? Add CSS to each div to be uniform width and length     
-//?     try float/clear
-//?     try inline-block
-//?     try flexbox
-//?     try css-grid
-//?         ** borders and margins can adjust the size of the squares **
-//? Add hover effect 
-//?     when mouse enters the div space, change the color 
-//?
-//? Add button to the top of the window for reset and clearing the current grid
-
 const containerDiv = document.querySelector('#container');
 
-
-
+//* Creating the grid of divs and adding class color
 for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
     div.classList.add('squareDiv');
     // div.style.backgroundColor = '#4082fc'; //test color
     // div.innerText = `${i}`;
-    console.log(div);
+    // console.log(div);
     containerDiv.appendChild(div);
     div.addEventListener('mouseover', (event) => {
-        console.log(event.target.style.backgroundColor);
+        // console.log(event.target.style.backgroundColor);
         div.classList.add('addColor')
     })
+
+
 }
+
+//* Reset/Clearing current grid'
+const clearGridBtn = document.querySelector('.clearGridBtn');
+const allDivs = document.querySelectorAll('.squareDiv');
+clearGridBtn.addEventListener('click', (e) => {
+    allDivs.forEach((e) => {
+        e.classList.remove('addColor');
+    })
+})
