e99965c24de418d5240041788c77f86f3972e4b8
ChrisJin <chrisjin1@gmail.com>
2021-05-09 19:05:57 -0400

    grid size Input, fixed reset

--- app.js
+++ app.js
@@ -1,26 +1,60 @@
 const containerDiv = document.querySelector('#container');
 
-//* Creating the grid of divs and adding class color
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.classList.add('squareDiv');
-    // div.style.backgroundColor = '#4082fc'; //test color
-    // div.innerText = `${i}`;
-    // console.log(div);
-    containerDiv.appendChild(div);
-    div.addEventListener('mouseover', (event) => {
-        // console.log(event.target.style.backgroundColor);
-        div.classList.add('addColor')
+//* Creating the grid of divs and adding class color (starting point)
+function defaultGrid() {
+    for (let i = 0; i < 256; i++) {
+        const div = document.createElement('div');
+        div.classList.add('squareDiv');
+        containerDiv.appendChild(div);
+        div.addEventListener('mouseover', () => {
+            div.classList.add('addColor')
+        })
+    }
+    containerDiv.style.setProperty('grid-template-columns', `repeat(16,1fr)`);
+}
+defaultGrid();
+
+//* Choosing a grid Size
+//? grid size will change 
+//? 1) number of boxes 
+//? 2) number of columns 
+//? 3) size of the boxes
+const gridSize = document.querySelector('.gridSize');
+let gridInput = "";
+gridSize.addEventListener('click', () => {
+    //! check to see if gridInput is a number, and is between 0 and 100
+
+    gridInput = parseInt(prompt('Choose number squares per side', 16));
+
+
+    gridSizeChange(gridInput);
+})
+
+function gridSizeChange(input) {
+    //? clear the current board of all divs
+    document.querySelectorAll('.squareDiv').forEach((e) => {
+        e.remove();
     })
+    let numberDivs = parseInt(input) * parseInt(input);
+    for (let j = 0; j < numberDivs; j++) {
+        const div = document.createElement('div');
+        div.classList.add('squareDiv');
+        containerDiv.appendChild(div);
+        div.addEventListener('mouseover', () => {
+            div.classList.add('addColor')
+        })
+        containerDiv.style.setProperty('grid-template-columns', `repeat(${input},1fr)`);
 
-
+    }
 }
 
 //* Reset/Clearing current grid'
 const clearGridBtn = document.querySelector('.clearGridBtn');
-const allDivs = document.querySelectorAll('.squareDiv');
-clearGridBtn.addEventListener('click', (e) => {
+clearGridBtn.addEventListener('click', resetColor);
+
+function resetColor() {
+    const allDivs = document.querySelectorAll('.squareDiv');
     allDivs.forEach((e) => {
         e.classList.remove('addColor');
     })
-})
+}
